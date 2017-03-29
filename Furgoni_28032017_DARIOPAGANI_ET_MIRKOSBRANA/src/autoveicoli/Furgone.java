da5f047b830b9969b3f668c92e68613b27d49e9a
DarioPagani <dariopag45@gmail.com>
2017-03-29 18:04:44 +0200

    Aggiunto il testo Modificate le classe

--- Furgoni_28032017_DARIOPAGANI_ET_MIRKOSBRANA/src/autoveicoli/Furgone.java
+++ Furgoni_28032017_DARIOPAGANI_ET_MIRKOSBRANA/src/autoveicoli/Furgone.java
@@ -2,5 +2,13 @@ package autoveicoli;
 
 public class Furgone extends Autoveicolo 
 {
-
+	private		float				velocitaMedia;
+	private		int					numeroPubblicita;
+	
+	public Furgone(int numeroKm, String targa, float velocitaMedia, int numeroPubblicita) 
+	{
+		super(numeroKm, targa);
+		this.velocitaMedia = velocitaMedia;
+		this.numeroPubblicita = numeroPubblicita;
+	}
 }
